d98019fef2cf341bbf3b6a9eab9f7beb4bb6dba2
AngelFmc <612110393@utsalamanca.edu.mx>
2023-01-18 10:20:36 -0600

    Se hizo

---
+++ components/user/login.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-card class="cardLogin">
+        <v-card-title>Login</v-card-title>
+        <v-card-text>
+            <v-form v-model="formularioValido">
+                <v-text-field label="Correo Electrónico" placeholder="Correo electronico" v-model="correoElectronico"/>
+            </v-form>
+        </v-card-text>
+        
+        <v-card-actions>
+            Boton para iniciar sesion
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+                formularioValido: false,
+                correoElectronico: ''
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .cardLogin{
+        background-color:#F9A825;
+        border-radius: 10px;
+        width: 500px;
+        height: 300px;
+    }
+</style>
